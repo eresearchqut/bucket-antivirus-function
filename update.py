d24f6e97db282e49745d6fa7b7496ba05632c8bf
Mitchel Haring <mitchel.haring@gmail.com>
2020-08-18 17:16:28 +1000

    stop downloading definitions from s3

--- update.py
+++ update.py
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 import boto3
+import os
+import shutil
 
 import clamav
 from common import AV_DEFINITION_PATH
@@ -24,22 +24,13 @@ from common import AV_DEFINITION_S3_PREFIX
 from common import CLAMAVLIB_PATH
 from common import get_timestamp
 
-
 def lambda_handler(event, context):
     s3 = boto3.resource("s3")
     s3_client = boto3.client("s3")
 
     print("Script starting at %s\n" % (get_timestamp()))
-    to_download = clamav.update_defs_from_s3(
-        s3_client, AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX
-    )
-
-    for download in to_download.values():
-        s3_path = download["s3_path"]
-        local_path = download["local_path"]
-        print("Downloading definition file %s from s3://%s" % (local_path, s3_path))
-        s3.Bucket(AV_DEFINITION_S3_BUCKET).download_file(s3_path, local_path)
-        print("Downloading definition file %s complete!" % (local_path))
+    # if os.path.exists(AV_DEFINITION_PATH):
+    #     shutil.rmtree(AV_DEFINITION_PATH)
 
     clamav.update_defs_from_freshclam(AV_DEFINITION_PATH, CLAMAVLIB_PATH)
     # If main.cvd gets updated (very rare), we will need to force freshclam
